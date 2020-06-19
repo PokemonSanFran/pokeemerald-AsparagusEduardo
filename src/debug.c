a9129886775422adf7346d68f60e840532fae761
Eduardo Quezada <eduardo602002@gmail.com>
2020-06-18 22:08:48 -0400

    Added checking and setting Wall Clock in Debug Menu.

--- src/debug.c
+++ src/debug.c
@@ -14,24 +14,28 @@
 #include "constants/songs.h"
 
 #define DEBUG_MAIN_MENU_HEIGHT 9
-#define DEBUG_MAIN_MENU_WIDTH 11
+#define DEBUG_MAIN_MENU_WIDTH 13
 
 static void Debug_ShowMenu(void (*HandleInput)(u8), struct ListMenuTemplate LMtemplate);
 void Debug_ShowMainMenu(void);
 static void Debug_DestroyMenu(u8);
 static void DebugAction_PlayBoo(u8);
 static void DebugAction_CheckSaveBlock(u8);
+static void DebugAction_CheckWallClock(u8);
+static void DebugAction_SetWallClock(u8);
 static void DebugAction_Cancel(u8);
-static void DebugAction_OpenSpaceManageMenu(u8);
+static void DebugAction_OpenUtilitiesMenu(u8);
 static void DebugAction_OpenSub1(u8);
 static void DebugTask_HandleMenuInput_Main(u8);
-static void DebugTask_HandleMenuInput_SpaceManage(u8);
+static void DebugTask_HandleMenuInput_Utilities(u8);
 static void DebugTask_HandleMenuInput_Sub1(u8);
 
 extern u8 Debug_CheckSaveBlock[];
+extern u8 PlayersHouse_2F_EventScript_SetWallClock[];
+extern u8 PlayersHouse_2F_EventScript_CheckWallClock[];
 
 enum DebugMenuOptions{
-    DEBUG_MENU_ITEM_SPACEMANAGE,
+    DEBUG_MENU_ITEM_UTILITIES,
     DEBUG_MENU_ITEM_OPTION_2,
     DEBUG_MENU_ITEM_OPTION_3,
     DEBUG_MENU_ITEM_OPTION_4,
@@ -42,12 +46,12 @@ enum DebugMenuOptions{
 
 enum DebugMenuOptions_SaveBlocks{
     SAVEBLOCKS_MENU_ITEM_CHECKSAVEBLOCK,
-    SAVEBLOCKS_MENU_ITEM_SAVEBLOCK2,
-    SAVEBLOCKS_MENU_ITEM_PKMNSTORAGE,
+    SAVEBLOCKS_MENU_ITEM_CHECKWALLCLOCK,
+    SAVEBLOCKS_MENU_ITEM_SETWALLCLOCK,
 };
 
 // Main Menu
-static const u8 gDebugText_SpaceManagement[] = _("Space Manage");
+static const u8 gDebugText_Utilities[] = _("Utilities");
 static const u8 gDebugText_Option2[] = _("Option 2");
 static const u8 gDebugText_Option3[] = _("Option 3");
 static const u8 gDebugText_Option4[] = _("Option 4");
@@ -55,7 +59,9 @@ static const u8 gDebugText_Option5[] = _("Option 5");
 static const u8 gDebugText_Option6[] = _("Option 6");
 static const u8 gDebugText_Cancel[] = _("Cancel");
 
-static const u8 gDebugText_SaveBlocks[] = _("SaveBlocks");
+static const u8 gDebugText_SaveBlockSpace[] = _("SaveBlock Space");
+static const u8 gDebugText_CheckWallClock[] = _("Check Wall Clock");
+static const u8 gDebugText_SetWallClock[] = _("Set Wall Clock");
 static const u8 gDebugText_None[] = _("None");
 
 // Sub Menu 1
@@ -63,7 +69,7 @@ static const u8 gDebugText__Sub1_OptionA[] = _("Option A");
 
 static const struct ListMenuItem sDebugMenuItems[] =
 {
-    [DEBUG_MENU_ITEM_SPACEMANAGE] = {gDebugText_SpaceManagement, DEBUG_MENU_ITEM_SPACEMANAGE},
+    [DEBUG_MENU_ITEM_UTILITIES] = {gDebugText_Utilities, DEBUG_MENU_ITEM_UTILITIES},
     [DEBUG_MENU_ITEM_OPTION_2] = {gDebugText_Option2, DEBUG_MENU_ITEM_OPTION_2},
     [DEBUG_MENU_ITEM_OPTION_3] = {gDebugText_Option3, DEBUG_MENU_ITEM_OPTION_3},
     [DEBUG_MENU_ITEM_OPTION_4] = {gDebugText_Option4, DEBUG_MENU_ITEM_OPTION_4},
@@ -74,9 +80,9 @@ static const struct ListMenuItem sDebugMenuItems[] =
 
 static const struct ListMenuItem sDebugMenu_SpaceManage_Items[] =
 {
-    [SAVEBLOCKS_MENU_ITEM_CHECKSAVEBLOCK] = {gDebugText_SaveBlocks, SAVEBLOCKS_MENU_ITEM_CHECKSAVEBLOCK},
-    [SAVEBLOCKS_MENU_ITEM_SAVEBLOCK2] = {gDebugText_None, SAVEBLOCKS_MENU_ITEM_SAVEBLOCK2},
-    [SAVEBLOCKS_MENU_ITEM_PKMNSTORAGE] = {gDebugText_None, SAVEBLOCKS_MENU_ITEM_PKMNSTORAGE},
+    [SAVEBLOCKS_MENU_ITEM_CHECKSAVEBLOCK] = {gDebugText_SaveBlockSpace, SAVEBLOCKS_MENU_ITEM_CHECKSAVEBLOCK},
+    [SAVEBLOCKS_MENU_ITEM_CHECKWALLCLOCK] = {gDebugText_CheckWallClock, SAVEBLOCKS_MENU_ITEM_CHECKWALLCLOCK},
+    [SAVEBLOCKS_MENU_ITEM_SETWALLCLOCK] = {gDebugText_SetWallClock, SAVEBLOCKS_MENU_ITEM_SETWALLCLOCK},
 };
 
 static const struct ListMenuItem sDebugMenu_Sub1_Items[] =
@@ -87,20 +93,20 @@ static const struct ListMenuItem sDebugMenu_Sub1_Items[] =
 
 static void (*const sDebugMenuActions[])(u8) =
 {
-    [DEBUG_MENU_ITEM_SPACEMANAGE] = DebugAction_OpenSpaceManageMenu,
-    [DEBUG_MENU_ITEM_OPTION_2] = DebugAction_OpenSub1,
+    [DEBUG_MENU_ITEM_UTILITIES] = DebugAction_OpenUtilitiesMenu,
+    [DEBUG_MENU_ITEM_OPTION_2] = NULL,
     [DEBUG_MENU_ITEM_OPTION_3] = NULL,
     [DEBUG_MENU_ITEM_OPTION_4] = NULL,
     [DEBUG_MENU_ITEM_OPTION_5] = NULL,
-    [DEBUG_MENU_ITEM_OPTION_6] = NULL,
+    [DEBUG_MENU_ITEM_OPTION_6] = DebugAction_OpenSub1,
     [DEBUG_MENU_ITEM_CANCEL] = DebugAction_Cancel
 };
 
 static void (*const sDebugMenu_SaveBlocks_Actions[])(u8) =
 {
     [SAVEBLOCKS_MENU_ITEM_CHECKSAVEBLOCK] = DebugAction_CheckSaveBlock,
-    [SAVEBLOCKS_MENU_ITEM_SAVEBLOCK2] = NULL,
-    [SAVEBLOCKS_MENU_ITEM_PKMNSTORAGE] = NULL,
+    [SAVEBLOCKS_MENU_ITEM_CHECKWALLCLOCK] = DebugAction_CheckWallClock,
+    [SAVEBLOCKS_MENU_ITEM_SETWALLCLOCK] = DebugAction_SetWallClock,
 };
 
 static void (*const sDebugMenu_Sub1_Actions[])(u8) =
@@ -213,7 +219,7 @@ static void DebugTask_HandleMenuInput_Main(u8 taskId)
         EnableBothScriptContexts();
     }
 }
-static void DebugTask_HandleMenuInput_SpaceManage(u8 taskId)
+static void DebugTask_HandleMenuInput_Utilities(u8 taskId)
 {
     void (*func)(u8);
     u32 input = ListMenu_ProcessInput(gTasks[taskId].data[0]);
@@ -250,34 +256,45 @@ static void DebugTask_HandleMenuInput_Sub1(u8 taskId)
     }
 }
 
-static void DebugAction_Cancel(u8 taskId)
+// Open sub-menus
+static void DebugAction_OpenUtilitiesMenu(u8 taskId)
 {
     Debug_DestroyMenu(taskId);
-    EnableBothScriptContexts();
+    Debug_ShowMenu(DebugTask_HandleMenuInput_Utilities, sDebugMenu_SpaceManage_ListTemplate);
 }
-
-static void DebugAction_OpenSpaceManageMenu(u8 taskId)
-{
-    Debug_DestroyMenu(taskId);
-    Debug_ShowMenu(DebugTask_HandleMenuInput_SpaceManage, sDebugMenu_SpaceManage_ListTemplate);
-}
-
 static void DebugAction_OpenSub1(u8 taskId)
 {
     Debug_DestroyMenu(taskId);
     Debug_ShowMenu(DebugTask_HandleMenuInput_Sub1, sDebugMenu_Sub1_ListTemplate);
 }
 
+// Actions
+static void DebugAction_Cancel(u8 taskId)
+{
+    Debug_DestroyMenu(taskId);
+    EnableBothScriptContexts();
+}
 static void DebugAction_PlayBoo(u8 taskId)
 {
     PlayBGM(MUS_BATTLE33);
 }
-
 static void DebugAction_CheckSaveBlock(u8 taskId)
 {
     Debug_DestroyMenu(taskId);
     ScriptContext2_Enable();
     ScriptContext1_SetupScript(Debug_CheckSaveBlock);
+}
+static void DebugAction_SetWallClock(u8 taskId)
+{
+    Debug_DestroyMenu(taskId);
+    ScriptContext2_Enable();
+    ScriptContext1_SetupScript(PlayersHouse_2F_EventScript_SetWallClock);
+}
+static void DebugAction_CheckWallClock(u8 taskId)
+{
+    Debug_DestroyMenu(taskId);
+    ScriptContext2_Enable();
+    ScriptContext1_SetupScript(PlayersHouse_2F_EventScript_CheckWallClock);
 }
 
 #endif
