62f2a13be318cb285df09957916f6e17edc8f5b1
Eduardo Quezada <eduardo602002@gmail.com>
2020-08-12 20:47:19 -0400

    Deletable HM moves.

--- src/party_menu.c
+++ src/party_menu.c
@@ -4729,6 +4729,7 @@ u16 ItemIdToBattleMoveId(u16 item)
 
 bool8 IsMoveHm(u16 move)
 {
+    /*
     u8 i;
 
     for (i = 0; i < NUM_HIDDEN_MACHINES; i++)
@@ -4736,6 +4737,7 @@ bool8 IsMoveHm(u16 move)
         if (sTMHMMoves[i + NUM_TECHNICAL_MACHINES] == move)
             return TRUE;
     }
+    */
     return FALSE;
 }
 
