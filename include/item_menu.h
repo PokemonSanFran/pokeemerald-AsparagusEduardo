9dd77a1b9a560f04c1140def56667f26a31ceb7e
Eduardo Quezada <eduardo602002@gmail.com>
2020-10-11 22:47:21 -0300

    Fix modern warning.

--- include/item_menu.h
+++ include/item_menu.h
@@ -70,7 +70,7 @@ struct BagMenuStruct
     u8 contextMenuItemsBuffer[4];
     u8 contextMenuNumItems;
     u8 numItemStacks[POCKETS_COUNT];
-    u8 numShownItems[6];
+    u8 numShownItems[POCKETS_COUNT + 1];
     s16 graphicsLoadState;
     u8 filler4[0xE];
     u8 pocketNameBuffer[32][32];
